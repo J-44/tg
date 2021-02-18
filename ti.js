36cd59ceca7d06344ed83c868ef71888ed79dca2
J-44 <74297043+J-44@users.noreply.github.com>
2021-02-18 16:01:11 +0530

    Add files via upload

---
+++ ti.js
@@ -0,0 +1,6 @@
+function send() {
+    player1 = document.getElementById("nu1").value;
+    player3 = document.getElementById("dig").value;
+    player2 = document.getElementById("nu2").value;
+    ans = nu1*nu2;
+}
